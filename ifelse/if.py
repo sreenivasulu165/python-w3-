30ecc12056031f987da6831b480b146cbd1da869
srinivasulu <galamgarisreenivasulu143@gmail.com>
2023-01-06 17:54:53 +0530

    python

---
+++ ifelse/if.py
@@ -0,0 +1,22 @@
+'''a=29
+b=39
+c=100
+if a<c or a>b:
+    pass'''
+    
+    #for loop
+    
+'''fruits=["appple","banana","mango"]
+for x in fruits:
+   
+    if x=="banana":
+        continue
+    print(x)'''
+    
+    
+  #range
+for x in range(2,12,2):
+    if x==6:break
+    print(x)  
+else:
+    print("good bye")
